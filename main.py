0e15317b5aa6f0986a43aecff663f7c29f4ad847
Alec McNamara <alecgmcnamara@gmail.com>
2022-05-03 20:42:59 +0100

    MicroPythonTest

---
+++ main.py
@@ -0,0 +1,15 @@
+from machine import Pin, PWM    # type: ignore
+from time import sleep
+
+frequency = 5000
+led = PWM(Pin(2), frequency)
+
+while True:
+  for duty_cycle in range(0, 1024, 3):
+    led.duty(duty_cycle)
+    sleep(0.005)
+  sleep(1)
+  for duty_cycle in range(1020, 0, -3):
+    led.duty(duty_cycle)
+    sleep(0.005)
+  sleep(1)
